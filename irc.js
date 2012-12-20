6b8a36543000044dd095963dbb1de7c08e8d8ed4
Tanner Rogalsky <the.t.dawg@gmail.com>
2012-12-19 20:59:06 -0500

    Flush out the irc client functionality a bit more and start using the redis client.

--- irc.js
+++ irc.js
@@ -1,7 +1,7 @@
 var Cyberzone = require('./src/cyberzone.js');
 
 var irc = require('irc');
-var client = new irc.Client('irc.mountai.net', 'admin', {
+var irc_client = new irc.Client('irc.mountai.net', 'admin', {
   channels: ['#gridhack'],
   userName: 'admin',
   realName: 'Administrator',
@@ -9,12 +9,19 @@ var client = new irc.Client('irc.mountai.net', 'admin', {
   debug: true
 });
 
-client.addListener('message', function (from, to, message) {
+var redis = require('redis');
+var redis_client = redis.createClient();
+var redis_keyspace = "cyberzone:irc:";
+
+irc_client.addListener('message', function (from, to, message) {
   console.log(from + ' => ' + to + ': ' + message);
 
   var matches = message.match(/^!(\S+)(.*)/);
-  var command = matches[1];
-  var args = matches[2].trim().split(" ");
+  var command, args;
+  if (matches) {
+    command = matches[1];
+    args = matches[2].trim().split(" ");
+  }
 
   if(typeof(commands[command]) === "function"){
     try{
@@ -25,12 +32,27 @@ client.addListener('message', function (from, to, message) {
   }
 });
 
-client.addListener('error', function(message) {
+irc_client.addListener('error', function(message) {
   console.log('error: ', message);
+});
+
+irc_client.addListener('ping', function(server) {
+  redis_client.ping();
+});
+
+irc_client.addListener('names', function(channel, nicks) {
+  console.log(nicks);
 });
 
 var commands = {};
 commands.upload = function(from, to, args){
-  console.log("test");
-  var game = new Cyberzone();
+  redis_client.get(redis_keyspace + "recruiting_game", function(err, id){
+    if (id === null) {
+      id = new Date().getTime();
+      redis_client.set(redis_keyspace + "recruiting_game", id);
+      redis_client.sadd(redis_keyspace + "active_games", id);
+    } else {
+      irc_client.say('#gridhack', "recruiting already in progress");
+    }
+  });
 };
